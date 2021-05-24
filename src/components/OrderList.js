b0d3221fdf224b7819a548ec0bf433980ffe7289
olivialin21 <s110819023@stu.ntue.edu.tw>
2021-05-24 17:29:25 +0800

    add order list

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -6,30 +6,48 @@ import { getOrderList } from "../actions"
 import { StoreContext } from "../store";
 
 export default function OrderList() {
-  const { state: { orderList , dispatch } } = useContext(StoreContext);
+  const { state: { userInfo, orderList }, dispatch } = useContext(StoreContext);
   // const antIcon = <LoadingOutlined style={{ fontSize: 80, color: "#8183ff" }} spin />;
 
   useEffect(() => {
     getOrderList(dispatch);
-  }, []);
+  }, [userInfo]);
 
   return (
+    <>
+    <h2 className="orderList-title">Order List</h2>
     <Row>
-      {(orderList === []) ?
+      {orderList.length === 0 ?
         (
-          <Col
-            md={{ span: 24 }}
-          >
-            <div>Cart is empty</div>
-          </Col>
+          <div className="orderList-list--empty">Your order list is empty.</div>
         ) : (
           <>
             {orderList.map( order => (
               <Col
-              md={{ span: 24 }}
+                xs={{ span: 20 , offset: 2 }}
+                sm={{ span: 20 , offset: 2 }}
+                lg={{ span: 12 , offset: 6 }}
               >
               <div className="card card-body">
-                <h2 style={{ color: 'white' }}>Order Summary</h2>
+                <h2 style={{ color: 'white' }}>{order.id}</h2>
+                {order.orderItems.map(item => (
+                  <li key={item.id} className="cart-item">
+                    <div className="cart-image">
+                      <img src={item.image} alt={item.name} />
+                    </div>
+                    <div className="cart-item-content">
+                      <div className="cart-name">{item.name}</div>
+                      <div className="product-qty">
+                        Qty: {item.qty}
+                      </div>
+                    </div>
+                    <div className="cart-item-end">
+                      <div className="cart-price">
+                        ${item.price * item.qty}
+                      </div>
+                    </div>
+                  </li>
+                ))}
                 <div className="row">
                   <div>Items</div>
                   <div>${order.itemsPrice}</div>
@@ -56,6 +74,7 @@ export default function OrderList() {
           </>
       )}
     </Row>
+    </>
   );
 }
 
